b59ae5cf447630a14ecd6e8dfac1f3e80d0a6cab
Devansh Goswami <devansh.goswami2004@gmail.com>
2026-07-15 03:07:05 +0530

    Add application source code, documentation, and screenshots

---
+++ application-code/app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'database-1.cfuwm6eq4rnf.ap-south-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'Qg4Js5QBDUqWWvR',
+    DB_DATABASE : 'webappdb'
+});
